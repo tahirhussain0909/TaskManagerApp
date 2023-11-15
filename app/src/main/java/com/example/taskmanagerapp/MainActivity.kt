62f89911c353525f9146507616ff652ce9f45bbd
tahirhussain0909 <tahirhussain5112@gmail.com>
2023-11-15 19:24:49 +0530

    Initialized and Completed the taskManager App

---
+++ app/src/main/java/com/example/taskmanagerapp/MainActivity.kt
@@ -0,0 +1,67 @@
+package com.example.taskmanagerapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.taskmanagerapp.ui.theme.TaskManagerAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TaskManagerAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    ManageTask()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun ManageTask(modifier: Modifier = Modifier) {
+    val image = painterResource(id = R.drawable.ic_task_completed)
+
+    Column(
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier
+    ) {
+        Image(
+            painter = image,
+            contentDescription = null,
+            modifier = Modifier.align(alignment = Alignment.CenterHorizontally)
+        )
+        Text(
+            text = stringResource(id = R.string.alert_message),
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier
+                .padding(top = 24.dp, bottom = 8.dp)
+                .align(alignment = Alignment.CenterHorizontally)
+        )
+        Text(
+            text = stringResource(id = R.string.appreciation_message),
+            fontSize = 16.sp,
+            modifier = Modifier.align(alignment = Alignment.CenterHorizontally)
+        )
+    }
+}
